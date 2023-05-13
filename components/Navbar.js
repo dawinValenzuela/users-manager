557f554aebf02860337efd30c189bdaab18f6ba9
Dawin Valenzuela <dawin.valenzuela@gmail.com>
2023-05-12 20:55:51 -0500

    add layout and navbar component

---
+++ components/Navbar.js
@@ -0,0 +1,11 @@
+import { Box } from '@chakra-ui/react';
+
+function Navbar() {
+  return (
+    <Box bg='gray.500' w='100%' p={4} color='white' mb={5}>
+      Users Manager
+    </Box>
+  );
+}
+
+export default Navbar;
--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,13 @@
 import '@src/styles/globals.css';
 import { ChakraProvider } from '@chakra-ui/react';
+import Layout from './layout';
 
 export default function App({ Component, pageProps }) {
   return (
     <ChakraProvider>
-      <Component {...pageProps} />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </ChakraProvider>
   );
 }
